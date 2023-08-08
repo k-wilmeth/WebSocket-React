45daea64b92bc5cc855ce838a768516c3d204bbc
KaipoWilmeth <kwilmeth@fanthreesixty.com>
2023-08-07 19:33:12 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const BACKEND_URL = 'http://localhost:<PORT>' // Replace <PORT> with the port your front-end client is running on
+
 function App() {
+
+  const [progress, setprogress] = useState(0);
+
+  const socketRef = useRef(null);
+  useEffect(() => {
+    if (!socketRef.current) {
+      socketRef.current = io.connect(BACKEND_URL);
+      console.log('connected to socket');
+
+      // Handle incoming messages from custom socket event "socket_data"
+      socketRef.current.on("socket_data", data => {
+        console.log(data);
+        setprogress(data.progress);
+      });
+
+      socketRef.current.on("connect_error", (err) => {
+        console.log(`connect_error due to ${err.message}`);
+      });
+    }
+
+    return () => socketRef?.current?.disconnect();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Progress: {progress}</p>
       </header>
     </div>
   );
